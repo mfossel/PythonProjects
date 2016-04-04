27bfb733e783df443373fc70e230e2591b69c59d
Michael Fossel <mikefossel@gmail.com>
2016-04-04 11:16:56 -0700

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+numbers = range(1,101)
+def fizzbuzz(numbers):
+    for number in numbers:
+             if number % 3 == 0 and number % 5 == 0
+            print "FizzBuzz"
+        elif number % 3 == 0:
+            print "Fizz"
+        elif number % 5 == 0:
+            print "Buzz"
+        else:
+            print each
+print fizzbuzz(numbers)
